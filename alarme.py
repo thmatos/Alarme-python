b322fbc82eed1d44c0f46294b3ac329008082bc0
Thiago Matos <matos.bsb@gmail.com>
2022-08-12 10:59:03 -0300

    Update alarm.py to windows
    Hello man, 
    
    Fui executar no windows e deu erro, consegui adaptar assim.
    
    Nem sei se o que estou fazendo aqui é legal, sou iniciante, mas se não for, só recusar. Não me leve a mal.

--- alarme.py
+++ alarme.py
@@ -1,15 +1,12 @@
-import datetime
-from playsound import playsound
-alarmHour = int(input("Horas:  "))
-alarmMin = int(input("Minutos:  "))
-alarmAm = input("am  /  pm:  " )
+from time import localtime
+import winsound
 
-if alarmAm=="pm":
-    alarmHour+=12
-    
-while True:
-    if alarmHour==datetime.datetime.now().hour and alarmMin==datetime.datetime.now().minute:
-        print("Acorda...")
-        playsound("samsung.mp3") 
-        break   
+h = input("coloque o horário desejado: ")
+m = input("coloque o minuto desejado: ")
+
+while True: 
+    if localtime().tm_hour == int(h) and localtime().tm_min == int(m):
+        print("Acooooooooorda...")
+        winsound.PlaySound("samsung.wav", winsound.SND_FILENAME)
+        break 
     
